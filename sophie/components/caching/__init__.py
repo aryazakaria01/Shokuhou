5a264b0f8d5e5aa486e5561fa858cee3ade1ee80
sabby <sabby.6@outlook.com>
2020-05-31 13:26:14 +0530

    Implement redisJSON

---
+++ sophie/components/caching/__init__.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2018 - 2020 MrYacha.
+# Copyright (C) 2020 Jeepeo
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# This file is part of Sophie.
+
+from rejson import Client
+from sophie.config import config
+
+
+redis = Client(host=config('redis/host', default='localhost'),
+               port=config('redis/port', default=6379),
+               decode_responses=True
+               )
